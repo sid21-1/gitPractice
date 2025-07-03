7acf246005e99b2a5c6ba8ddc5ee49b2a033c838
Ram95911 <ramchandrakoli545@gmail.com>
2025-07-03 15:12:56 +0530

    changes are made

--- Demo.java
+++ Demo.java
@@ -2,5 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Demo Print");
         System.out.println("Demo Print");
+
+        System.out.println("Demo Print change");
     }
 }

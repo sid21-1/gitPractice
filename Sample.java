d794bd700050ba4681bd6b6b6dbee3a807da9f44
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2025-07-03 12:58:49 +0530

    Added printing statement in Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 class Sample {
     public static void main(String[] args) {
         System.out.println("Learning git and github");
+        System.out.println("Learning git and github");
     }
 }
